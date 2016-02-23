c0515a523f578da33c8836fbe3f86ecf605b14bc
Jens Buss <jens.buss@udu.edu>
2016-02-23 14:28:08 +0100

    added logspace, now the plots make sense

--- plot_effective_area.py
+++ plot_effective_area.py
@@ -2,7 +2,7 @@
 """Plot effective area for a dataset from HDF5 files
 
 Usage:
-    plot_effective_area.py <datafile> [options]
+    plot_effective_area.py <datafile> <datafile2> [options]
 
 Options:
     --tablename=<name>      [default: table]
@@ -25,48 +25,72 @@ import default_cuts as dc
 import gc
 
 logger  = logging.getLogger(__name__)
-
-def expE(E,x):
-    return E**x
-
+7
 def integrateEnergySpectrum(e_min, e_max, gamma):
-    upper = expE( e_max, gamma+1) / gamma+1
-    lower = expE( e_min, gamma+1) / gamma+1
+    upper = np.power( e_max, gamma+1) / gamma+1
+    lower = np.power( e_min, gamma+1) / gamma+1
     return upper-lower
 
-def calculate_a_eff_bin(n_trigger, max_impact, E_bin_low, e_bin_high, E_min, E_max, gamma):
-    N_0 =   12e6
-    N_0 =   1
-    N_0 *=  integrateEnergySpectrum(E_bin_low, e_bin_high, gamma)
+def calculate_a_eff_bin(n_trigger, max_impact, E_bin_low, e_bin_high, E_min, E_max, gamma, nCorsika = 12e6):
+    N_0 =   nCorsika
     N_0 /=  integrateEnergySpectrum(E_min, E_max, gamma)
+    N_0 *=  integrateEnergySpectrum(E_bin_low, e_bin_high, gamma)
 
     return n_trigger / N_0 * np.pi * max_impact**2
 
-def calc_a_eff(true_energy, bins, min_e=None, max_e=None, max_impact=270, gamma=-2.7):
-    e_mc = np.log10(true_energy)
+def calc_a_eff(e_mc, nBins, min_e=None, max_e=None, max_impact=170, gamma=-2.7, nCorsika = 12e6):
+    """Calculates the effective collection area for each given energy bin
+
+    Keyword arguments:
+
+    e_mc -- monte carlo energy
+
+    nBins        -- number of energy bins in range
+
+    min_e       -- min. simulated energy in GeV
+
+    max_e       -- max. simulated energy in GeV
+
+    max_impact  -- maximally simulated impact parameter in m
+
+    gamma       -- spectral index
+    """
+
     if min_e is None:
         min_e = np.min(e_mc)
     if max_e is None:
         max_e = np.max(e_mc)
 
-    bin_width = (max_e - min_e)/bins
-    logger.info("Number of bins: {}".format(bins))
+    logger.info("Energy range {} GeV to {} GeV".format(min_e, max_e))
+    logger.info("Number of bins: {}".format(nBins))
 
-    bin_middles = np.empty(bins)
+
+    bins = np.logspace(np.log10(min_e), np.log10(max_e), nBins)
+    logger.debug("Bin width: {}".format(bins))
+
+    bin_middles = np.empty(nBins)
     bin_middles[:] = np.nan
+    bin_widths = np.empty(nBins)
+    bin_widths[:] = np.nan
 
-    A_eff = np.empty(bins)
+    A_eff = np.empty(nBins)
     A_eff[:] = np.nan
 
-    A_eff_err = np.empty(bins)
+    A_eff_err = np.empty(nBins)
     A_eff_err[:] = np.nan
 
-    n_trigger = np.empty(bins)
+    n_trigger = np.empty(nBins)
     n_trigger[:] = np.nan
 
-    for i in range(bins):
-        bin_low_edge  = bin_width*i + min_e
-        bin_high_edge = bin_width*(i+1) + min_e
+    bin_low_edge  = min_e
+    bin_high_edge = 0
+
+    for i in range(1, nBins):
+        bin_low_edge  = bins[i-1]
+        bin_high_edge = bins[i]
+        if bin_high_edge > max_e:
+            bin_high_edge = max_e
+        #sum up all prio bin widths
 
         mask1 = e_mc < bin_high_edge
         mask2 = e_mc >= bin_low_edge
@@ -77,7 +101,8 @@ def calc_a_eff(true_energy, bins, min_e=None, max_e=None, max_impact=270, gamma=
             logger.info("no entries for given mask {} <= emc < {}".format( '%.2E' % bin_low_edge, '%.2E' % bin_high_edge))
             continue
 
-        bin_middles[i] = bin_width * (i+0.5) + min_e
+        bin_widths[i]  = (bin_high_edge - bin_low_edge)/2
+        bin_middles[i] = bin_low_edge + bin_widths[i]
 
         energy_bin = e_mc[mask]
         #print(energy_bin)
@@ -85,15 +110,18 @@ def calc_a_eff(true_energy, bins, min_e=None, max_e=None, max_impact=270, gamma=
 
         A_eff[i] = calculate_a_eff_bin(n_trigger[i], max_impact,
                                         bin_low_edge, bin_high_edge,
-                                        min_e, max_e, gamma)
+                                        min_e, max_e, gamma, nCorsika)
+        #A_eff[i] *= np.sum(energy_bin)
 
         A_eff_err[i] = np.sqrt(n_trigger[i])
 
-        logger.debug("Aeff={}m² with {} entries for given range {} <= emc < {}".format(
+        logger.debug("Aeff={}m², N_tr = {}, range {} <= emc < {}, bin width={}".format(
                                         '%.2E' % A_eff[i],
                                         '%.2E' % n_trigger[i],
                                         '%.2E' % bin_low_edge,
-                                        '%.2E' % bin_high_edge))
+                                        '%.2E' % bin_high_edge,
+                                        '%.2E' % bins[i],
+                                        ))
 
     return_dict = dict(
         range       = np.array([min_e,max_e]),
@@ -101,7 +129,8 @@ def calc_a_eff(true_energy, bins, min_e=None, max_e=None, max_impact=270, gamma=
         Aeff_res    = np.array(A_eff_err),
         nTrigger    = np.array(n_trigger),
         bin_middles = np.array(bin_middles),
-        bin_width   = np.array(bin_width)
+        bin_widths = np.array(bin_widths),
+        bins   = np.array(bins)
     )
 
     return return_dict
@@ -112,7 +141,8 @@ args = docopt(__doc__)
 logging.captureWarnings(True)
 logging.basicConfig(format=('%(asctime)s - %(name)s - %(levelname)s - ' +  '%(message)s'), level=logging.DEBUG)
 
-datafile    = args["<datafile>"]
+
+datafiles    = (args["<datafile>"], args["<datafile2>"])
 #outputfile  = args["<outputfile>"]
 
 tablename   = args["--tablename"]
@@ -120,20 +150,27 @@ tablename   = args["--tablename"]
 cuts            = args["--cuts"]
 default_cuts    = args["--default_cuts"]
 
-print("loading data file")
-data_df = pd.read_hdf(datafile, tablename)
+for datafile in datafiles:
+    print("loading data file")
+    data_df = pd.read_hdf(datafile, tablename)
 
-e_mc = data_df["MCorsikaEvtHeader.fTotalEnergy"]
+    e_mc = data_df["MCorsikaEvtHeader.fTotalEnergy"]
 
-effectiveArea = calc_a_eff(e_mc, 20)
+    logger.info("No events in File {}".format(len(data_df)))
 
-ax = plt.subplot(1,1,1)
-plt.errorbar(effectiveArea["bin_middles"],
-            effectiveArea["Aeff"],
-            xerr=effectiveArea["bin_width"]/2,
-            yerr=effectiveArea["Aeff_res"],
-            fmt="o")
 
-ax.set_yscale('log')
-#ax.set_xscale('log')
+    effectiveArea = calc_a_eff(e_mc, 20, min_e=200, max_e=50e3)
+
+    ax = plt.subplot(1,1,1)
+    a = plt.errorbar(effectiveArea["bin_middles"],
+                effectiveArea["Aeff"],
+                xerr=effectiveArea["bin_widths"],
+                yerr=effectiveArea["Aeff_res"],
+                fmt="o", label=datafile[-13:-9])
+
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    ax.set_xlabel("log10(E / GeV)")
+    ax.set_ylabel("log10(A_eff / mˆ2)")
+plt.legend()
 plt.show()
