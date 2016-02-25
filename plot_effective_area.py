4e300199cd186d657be5719160502f274b638b99
Jens Buss <jens.buss@udu.edu>
2016-02-25 16:58:29 +0100

    added cuts

--- plot_effective_area.py
+++ plot_effective_area.py
@@ -37,6 +37,8 @@ logging.captureWarnings(True)
 logging.basicConfig(format=('%(asctime)s - %(name)s - %(levelname)s - ' +  '%(message)s'), level=logging.DEBUG)
 
 
+
+
 datafiles    = args["<datafiles>"]
 outputfile  = args["<outputfile>"]
 
@@ -48,13 +50,18 @@ feature         = args["--feature"]
 unit            = args["--unit"]
 pattern            = args["--pattern"].split(",")
 
+data_df_list = []
+
+cuts = dc.cuts["ICRC2015_pre_Xtalk"]
+cuts = " & ".join(cuts)
+
 # c_cycle = cycler('color', ['red', 'b', 'black', 'g', 'yellow', 'orange','darkgrey', 'c', 'm'])
-fig = plt.figure()
+fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
 for datafile in datafiles:
     print("loading data file")
     data_df = pd.read_hdf(datafile, tablename)
-
-    ax = plt.subplot(1,1,1)
+    data_df = data_df.query(cuts)
+    data_df_list.append(data_df)
     # ax.set_prop_cycle(c_cycle)
 
     logger.info("{} events in File".format(len(data_df)))
@@ -67,11 +74,29 @@ for datafile in datafiles:
         data_df = data_df.query(query)
 
     e_mc = data_df["MCorsikaEvtHeader.fTotalEnergy"]
-    plot_eff_area(ax, e_mc, label=label )
-    ax.set_xlabel("$log_{10}$($E_{True}$ / $\si{\GeV}$)")
-    ax.set_ylabel("$log_{10}$($A_{eff}$ / $\si{\meter}^2$)")
-    ax.set_ylim([2e2,2e5])
+    effA_result = plot_eff_area(ax[0], e_mc, label=label )
+    ax[0].set_xlabel("$\log_{10} (E_\mathrm{True} / \si{\GeV})$")
+    ax[0].set_ylabel("$\log_{10} (A_\mathrm{eff} / \si{\meter}^2)$")
+    # ax.set_ylim([2e2,2e5])
 
-plt.legend(loc = 'lower right')
+    plot_nTriggers(ax[1], effA_result, label=label )
+
+fig.subplots_adjust(hspace=0.06)
+ax[0].legend(loc = 'lower right')
+ax[1].legend()
+
 # plt.show()
 fig.savefig(outputfile)
+
+
+fig = plt.figure()
+for data_df in data_df_list:
+    val = os.path.basename(datafile).split(pattern[0])[-1].split(pattern[1])[0]
+    label= "{} {} {}".format(feature, val, unit)
+    hist, edges, patches = plt.hist(np.log10(data_df["MCorsikaEvtHeader.fTotalEnergy"]), log=True, bins=20, label=label, alpha=0.5, histtype="step")
+    xy  = patches[0].get_xy()
+    xy[:, 1][xy[:, 1] == 0] = 0.1
+    patches[0].set_xy(xy)
+
+fig.get_axes()[0].legend()
+fig.savefig("E_True"+outputfile)
