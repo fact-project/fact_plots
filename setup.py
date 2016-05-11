b472150cf551ebc5b52861e0bc0ae9a2279ef1c1
Jens Buss <jens.buss@udu.edu>
2016-05-11 16:32:16 +0200

    renamed fact_data_mc_compare

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='jens.buss@tu-dortmund.de',
     license='BEER',
     packages=[
-        'fact-plotting',
+        'fact_plotting',
     ],
     # dependency_links = ['git+https://github.com/mackaiver/gridmap.git#egg=gridmap'],
     package_data={
@@ -22,22 +22,19 @@ setup(
         'python-dateutil',  # in anaconda
         'pytz',             # in anaconda
         'tables',           # needs to be installed by pip for some reason
-        'hdf5',
+        # 'hdf5',
         'click',
         'docopt',
         'datetime',
         'numexpr',
         'IPython',
-        'logging',
         'pytest', # also in  conda
         # 'gridmap>=0.13.1', install from https://github.com/mackaiver/gridmap'
     ],
    zip_safe=False,
    entry_points={
     'console_scripts': [
-        'plot_data_mc_compare.py = fact-plotting.scripts.plot_data_mc_compare.py:main',
+        'fact_plot_data_mc_compare = fact_plotting.scripts.plot_data_mc_compare:main',
     ],
   }
 )
-Status API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Contact Help
