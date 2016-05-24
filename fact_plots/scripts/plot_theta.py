4d58afacd373c92e14e0c0a274b34915fb414d1c
Jens Buss <jens.buss@udu.edu>
2016-05-24 10:41:29 +0200

    added box for information on the plot

--- fact_plots/scripts/plot_theta.py
+++ fact_plots/scripts/plot_theta.py
@@ -34,6 +34,7 @@ def main():
 
     tablename   = args["--tablename"]
     bins        = int(args["--bins"])
+    title       = args["--title"]
     threshold   = 0.5
 
     alpha = 1.0
@@ -73,25 +74,65 @@ def main():
     df_background = df.query('(background_prediction > {})'.format(best_threshold))
     signal_theta = df_signal['signal_theta'].values
     background_theta = df_background['background_theta'].values
-    hist_signal, edges, _ = plt.hist(signal_theta, bins=bins, alpha=0.6, label='On region', range=(0,0.3))
-    hist_signal, _, _ = plt.hist(background_theta, bins=edges, alpha=0.6, label='Off region', range=(0,0.3))
 
-    # hist_background, edges = np.histogram(background_theta, bins=edges)
-    # hist_background = hist_background * alpha
-    # plt.bar(edges[:-1], hist_background, width=edges[1]-edges[0], alpha=0.6, label='Off region')
-    # t_off = pd.concat([df_background[["Theta_Off_1","Theta_Off_2","Theta_Off_3","Theta_Off_4", "Theta_Off_5"]]])
-    # y, edges = np.histogram(t_off, bins=edges)
+    excess_events = len(signal_theta) - len(background_theta)
+    background_events = len(background_theta)
+
+    theta_max = 0.3
+    bins = np.linspace(0, theta_max, bins)
+
+    #Define measures for plot and info box
+    info_left = 0.0
+    info_height = 0.3
+    info_width = 1.0
+    info_bottom = 1.
+    info_top    = info_bottom + info_height
+    info_right  = info_left + info_width
+    plot_height = 1. - info_height
+    plot_width  = theta_max
+
+
+    fig = plt.figure()
+    fig.subplots_adjust(top=plot_height)
+    ax = fig.gca()
+
+    #Plot the Theta2 Distributions
+    sig_x, sig_y, sig_norm = histpoints(signal_theta, bins=bins, xerr='binwidth', label='On', fmt='none', ecolor='b', capsize=0 )
+    back_x, back_y, back_norm = histpoints(background_theta, bins=bins, xerr='binwidth', label='Off', fmt='none', ecolor='r', capsize=0)
+
+    #Fill area underneeth background
+    ax.fill_between(back_x, back_y, 0, facecolor='grey', alpha=0.2, linewidth=0.0)
+
+    #Mark theta cut with a line0.5*(info_left+info_right),
+    ax.axvline(x=best_theta_cut, linewidth=1, color='k', linestyle='dashed')
+
     # embed()
-    # y =y * alpha
-    # plt.bar(edges[:-1], y, width=edges[1]-edges[0], alpha=0.6, color='gray', label="huhrensohn")
+
+    # Draw info Box
+    p = patches.Rectangle( (info_left, 1.), info_width, info_height, fill=True, transform=ax.transAxes, clip_on=False, facecolor='0.9', edgecolor='black')
+    ax.add_patch(p)
+
+    info_text = 'Significance: {:.2f}\n'.format(best_significance)
+    info_text += 'Confidence Cut: {:.2f}, Theta Sqare Cut: {:.2f} \n'.format(best_threshold, best_theta_cut)
+    info_text += '{} excess events, {} background events \n'.format(excess_events, background_events)
 
 
+    ax.text(0.5*(info_left+info_right), 0.5*(info_top+info_bottom)-0.05, info_text,
+            horizontalalignment='center', verticalalignment='center', fontsize=12, transform=ax.transAxes)
 
-
+    ax.text(0.5*(info_left+info_right), (info_top), title,
+                bbox={'facecolor':'white', 'pad':10},
+                horizontalalignment='center',
+                verticalalignment='center',
+                fontsize=14, color='black',
+                transform=ax.transAxes)
 
         # hist_background, edges , _ = plt.hist(df_background.values, bins=edges, alpha=0.6, label='Off region')
-    plt.xlabel("Theta^2 in mm^2")
-    plt.legend()
+    # plt.xlabel("$//Theta^2 in mm^2$")
+    plt.xlabel("Theta^2 / mm^2")
+    plt.ylabel("Counts")
+
+    plt.legend(fontsize=12)
     # plt.show()
     plt.savefig(out)
 
