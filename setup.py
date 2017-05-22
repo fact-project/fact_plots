de5fd55dfe5a3f216409f82b2eb56a4526d4000b
Kai B <kai.bruegge@tu-dortmund.de>
2017-05-22 13:18:40 +0200

    add theta plot script to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fact_plots',
-    version='0.0.1',
+    version='0.0.2',
     description='a collection of plotting scrtipts for data of FACT',
     url='https://github.com/fact-project/fact_plots',
     author='Jens Buss',
@@ -10,12 +10,8 @@ setup(
     license='BEER',
     packages=[
         'fact_plots',
-            'fact_plots/scripts',
+        'fact_plots.scripts',
     ],
-    # dependency_links = ['git+https://github.com/mackaiver/gridmap.git#egg=gridmap'],
-    package_data={
-        # 'erna': ['resources/*'],
-    },
     install_requires=[
         'pandas',           # in anaconda
         'numpy',            # in anaconda
@@ -24,17 +20,16 @@ setup(
         'pytz',             # in anaconda
         'tables',           # needs to be installed by pip for some reason
         # 'hdf5',
+        'click',
+        'pyfact>=10.4',
         'h5py',
         'scipy',
         'click',
         'docopt',
         'datetime',
         'numexpr',
-        'IPython',
         'pytest', # also in  conda
         'matplotlib-hep==0.1.0'
-        #'matplotlib-hep', install from https://github.com/ibab/matplotlib-hep'
-
     ],
    zip_safe=False,
    entry_points={
@@ -42,8 +37,7 @@ setup(
         'fact_plot_data_mc_compare = fact_plots.scripts.plot_data_mc_compare:main',
         'fact_plot_effective_area = fact_plots.scripts.plot_effective_area:main',
         'fact_plot_ped_std_mean_cureent_mean = fact_plots.scripts.plot_ped_std_mean_cureent_mean:main',
-        'fact_plot_theta = fact_plots.scripts.plot_theta:main',
-
+        'fact_plot_theta = fact_plots.scripts.plot_theta_squared:main',
     ],
   }
 )
