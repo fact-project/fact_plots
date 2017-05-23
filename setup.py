1cb17d6a14c7ec9f72915679bdaedd8a0f613d10
Kai B <kai.bruegge@tu-dortmund.de>
2017-05-23 12:26:25 +0200

    bump version and add entrypoints

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fact_plots',
-    version='0.0.2',
+    version='0.0.3',
     description='a collection of plotting scrtipts for data of FACT',
     url='https://github.com/fact-project/fact_plots',
     author='Jens Buss',
@@ -21,7 +21,7 @@ setup(
         'tables',           # needs to be installed by pip for some reason
         # 'hdf5',
         'click',
-        'pyfact>=10.4',
+        'pyfact>=0.10.4',
         'h5py',
         'scipy',
         'click',
@@ -36,7 +36,8 @@ setup(
     'console_scripts': [
         'fact_plot_data_mc_compare = fact_plots.scripts.plot_data_mc_compare:main',
         'fact_plot_effective_area = fact_plots.scripts.plot_effective_area:main',
-        'fact_plot_ped_std_mean_cureent_mean = fact_plots.scripts.plot_ped_std_mean_cureent_mean:main',
+        'fact_plot_excess_rate = fact_plots.scripts.plot_excess_rate:main',
+        'fact_plot_ped_std_mean_curent_mean = fact_plots.scripts.plot_ped_std_mean_curent_mean:main',
         'fact_plot_theta = fact_plots.scripts.plot_theta_squared:main',
     ],
   }
