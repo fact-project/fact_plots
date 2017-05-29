c3f3b01694781d10239cd17efc778ef71eaceb05
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-05-29 19:06:47 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='fact_plots',
-    version='0.0.3',
+    version='0.0.4',
     description='a collection of plotting scrtipts for data of FACT',
     url='https://github.com/fact-project/fact_plots',
-    author='Jens Buss',
+    author='Kai Brügge, Jens Buss, Maximilian Nöthe',
     author_email='jens.buss@tu-dortmund.de',
     license='BEER',
     packages=[
@@ -19,7 +19,6 @@ setup(
         'python-dateutil',  # in anaconda
         'pytz',             # in anaconda
         'tables',           # needs to be installed by pip for some reason
-        # 'hdf5',
         'click',
         'pyfact>=0.10.4',
         'h5py',
@@ -28,17 +27,17 @@ setup(
         'docopt',
         'datetime',
         'numexpr',
-        'pytest', # also in  conda
+        'pytest',
         'matplotlib-hep==0.1.0'
     ],
-   zip_safe=False,
-   entry_points={
-    'console_scripts': [
-        'fact_plot_data_mc_compare = fact_plots.scripts.plot_data_mc_compare:main',
-        'fact_plot_effective_area = fact_plots.scripts.plot_effective_area:main',
-        'fact_plot_excess_rate = fact_plots.scripts.plot_excess_rate:main',
-        'fact_plot_ped_std_mean_curent_mean = fact_plots.scripts.plot_ped_std_mean_curent_mean:main',
-        'fact_plot_theta = fact_plots.scripts.plot_theta_squared:main',
-    ],
-  }
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'fact_plot_data_mc_compare = fact_plots.scripts.plot_data_mc_compare:main',
+            'fact_plot_effective_area = fact_plots.scripts.plot_effective_area:main',
+            'fact_plot_excess_rate = fact_plots.scripts.plot_excess_rate:main',
+            'fact_plot_ped_std_mean_curent_mean = fact_plots.scripts.plot_ped_std_mean_curent_mean:main',
+            'fact_plot_theta_squared = fact_plots.scripts.plot_theta_squared:main',
+        ],
+    }
 )
