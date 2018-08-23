06d2f0c4ddfdcb27cf6fb060ef02a2bcea3ce296
Jens Buss <jens.buss@online.de>
2018-08-23 10:35:56 +0200

    use lower snail case in default cuts

--- fact_plots/default_cuts.py
+++ fact_plots/default_cuts.py
@@ -1,53 +1,53 @@
 cuts = dict()
 
 cuts["uncutted"] = [
-    "Size > 0"
+    "size > 0"
     ]
 
 cuts["qualitycuts"] = [
-        "Size > 65",
-        "Leakage < 0.5",
-        "Leakage2 < 0.85",
-        "numIslands < 8",
-        "Width < 45",
-        "Length < 60",
-        # "Concentration_onePixel > 0.01",
-        # "Concentration_twoPixel > 0.02",
-        "ConcCore > 0",
-        # "concCOG > 0.02"
+        "size > 65",
+        "leakage1 < 0.5",
+        "leakage2 < 0.85",
+        "num_islands < 8",
+        "width < 45",
+        "length < 60",
+        # "concentration_one_pixel > 0.01",
+        # "concentration_two_pixel > 0.02",
+        "concentration_core > 0",
+        # "concentration_cog > 0.02"
 ]
 
 cuts["precuts"] = [
-        # "Size > 60",
-        "Leakage < 0.09",
-        "Leakage2 < 0.15",
-        "numIslands < 4",
-        "Width < 13.25",
-        "Length < 37",
-        "numPixelInShower > 10",
-        "Concentration_onePixel > 0.0325",
-        "Concentration_twoPixel > 0.063",
-        # "ConcCore > 0",
-        "concCOG > 0.055",
+        # "size > 60",
+        "leakage1 < 0.09",
+        "leakage2 < 0.15",
+        "num_islands < 4",
+        "width < 13.25",
+        "length < 37",
+        "num_pixel_in_shower > 10",
+        "concentration_one_pixel > 0.0325",
+        "concentration_two_pixel > 0.063",
+        # "concentration_core > 0",
+        "concentration_cog > 0.055",
 ]
 
 cuts["analysis4_pre"] = [
-    "Leakage < 0.6",
-    "Leakage2 < 0.85",
-    "numIslands < 8",
-    "numPixelInShower >= 10",
-    "Width < 35",
-    "Length < 70",
+    "leakage1 < 0.6",
+    "leakage2 < 0.85",
+    # "num_islands < 8",
+    "num_pixel_in_shower >= 10",
+    "width < 35",
+    "length < 70",
 ]
 
 cuts["ICRC2015_pre_Xtalk"] = [
-        "Size > 60",
-         "Leakage <= 0.09",
-         "Leakage2 <= 0.15",
-         "numIslands <2",
-         "Width < 10",
-         "Length < 28",
-        #  "Width*Length*3.141/pow(log10(Size),2) < 80", #area vs. size cut
-        "Concentration_onePixel > 0.044",
-        "Concentration_twoPixel > 0.09",
+        "size > 60",
+         "leakage1 <= 0.09",
+         "leakage2 <= 0.15",
+         "num_islands <2",
+         "width < 10",
+         "length < 28",
+        #  "width*length*3.141/pow(log10(size),2) < 80", #area vs. size cut
+        "concentration_one_pixel > 0.044",
+        "concentration_two_pixel > 0.09",
 ]
